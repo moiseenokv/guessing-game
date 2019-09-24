1b7bca60224a7b800865ec086933258ac86c049b
Vitali Moiseenok <moiseenokv@gmail.com>
2019-09-24 13:11:40 +0300

    feat: add guessing game class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+        this.middle
+    }
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
     }
 
     guess() {
-
+        this.middle = Math.ceil((this.min+this.max)/2);
+        return this.middle; 
     }
 
     lower() {
-
+        this.max = this.middle
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
